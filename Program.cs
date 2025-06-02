f9352beabae5f97654c9316bb34be02ee41a37f6
TvMroc <1207837@student.roc-nijmegen.nl>
2025-06-02 10:11:42 +0200

    messanger

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace spotivy_app
+﻿using spotivy_app.spotivy;
+
+namespace spotivy_app
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Messanger Messanger = new Messanger();
+            Messanger.SendMessage("Welcome to Spotivy! This is a simple Spotify client for Linux, built with C#. Enjoy your music experience!");
         }
     }
 }
---
+++ spotivy/Messanger.cs
@@ -0,0 +1,48 @@
+﻿namespace spotivy_app.spotivy
+{
+    class Messanger
+    {
+        public static void SendMessage(string message)
+        {
+            MessageBorder(message.Length);
+            Console.WriteLine("| " + message+ " |");
+            MessageBorder(message.Length);
+        }
+
+        public static void Conversation(string[] messages, Option[][] options)
+        {
+            int LongestMessageLength = 0;
+            foreach (var message in messages)
+            {
+                if (message.Length > LongestMessageLength) LongestMessageLength = message.Length;
+            }
+            MessageBorder(LongestMessageLength);
+            for (int i = 0; i > messages.Length; i++)
+            {
+                Message(messages[i], LongestMessageLength);
+
+            }
+
+            MessageBorder(LongestMessageLength);
+        }
+
+        private static void MessageBorder(int length)
+        {
+            Console.Write("+-");
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("-+");
+        }
+        private static void Message(string message, int length)
+        {
+            Console.WriteLine("| " + message);
+            for (int i = 0; i < length - message.Length; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(" |");
+        }
+    }
+}
