6eb3ecebefd6a6b0b6fa818dfd528dfe9ec98d3b
TvMroc <1207837@student.roc-nijmegen.nl>
2025-06-02 10:46:03 +0200

    messanger Conversation

--- spotivy/Messanger.cs
+++ spotivy/Messanger.cs
@@ -1,26 +1,54 @@
-﻿namespace spotivy_app.spotivy
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace spotivy_app.spotivy
 {
     class Messanger
     {
         public static void SendMessage(string message)
         {
             MessageBorder(message.Length);
-            Console.WriteLine("| " + message+ " |");
+            Console.WriteLine("| " + message + " |");
             MessageBorder(message.Length);
         }
 
         public static void Conversation(string[] messages, Option[][] options)
         {
             int LongestMessageLength = 0;
-            foreach (var message in messages)
+            // messages and options has to be the same length
+            for (int i = 0; i < messages.Length; i++)
             {
-                if (message.Length > LongestMessageLength) LongestMessageLength = message.Length;
-            }
-            MessageBorder(LongestMessageLength);
-            for (int i = 0; i > messages.Length; i++)
-            {
-                Message(messages[i], LongestMessageLength);
+                if (messages[i].Length > LongestMessageLength) LongestMessageLength = messages[i].Length;
 
+                foreach (var opt in options[i])
+                {
+                    if (opt.Label.Length + 2 > LongestMessageLength) LongestMessageLength = opt.Label.Length + 5;
+                }
+            }
+
+            for (int i = 0; i < messages.Length; i++) 
+            {
+                MessageBorder(LongestMessageLength);
+                Message(messages[i], LongestMessageLength);
+                Message("To choose an option enter then number infront of an option.", LongestMessageLength);
+                // show each option with it's number
+                for (int o = 0; o < options[i].Length; o++) 
+                {
+                    var label = options[i][o].Label;
+                    Message(label + " " + (o > 9 ? ".  " : ". "), LongestMessageLength);
+                }
+                // loop until user gives valid input, run the selected options function
+                bool validInput = false;
+                while (!validInput)
+                {
+                    if (int.TryParse(Console.ReadLine(), out int userInput))
+                    {
+                        if (userInput >= 0 && userInput < options[i].Length)
+                        {
+                            options[i][userInput].Action.Invoke();
+                            validInput = true;
+                        }
+                    }
+                }
             }
 
             MessageBorder(LongestMessageLength);
@@ -35,9 +63,10 @@
             }
             Console.WriteLine("-+");
         }
+
         private static void Message(string message, int length)
         {
-            Console.WriteLine("| " + message);
+            Console.Write("| " + message);
             for (int i = 0; i < length - message.Length; i++)
             {
                 Console.Write(" ");
