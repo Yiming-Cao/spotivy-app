797ef2c5cc0d4a83482509d5ca9c05e93bdb02fd
TvMroc <1207837@student.roc-nijmegen.nl>
2025-06-05 16:41:27 +0200

    Added songs artists and album

---
+++ spotivy/Album.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spotivy_app.spotivy
+{
+    class Album : SongCollection
+    {
+        public List<Artist> Artists { get; set; }
+
+        public Album(List<Artist> artists, string title, List<Song> songs) : base(title)
+        {
+            Artists = artists;
+        }
+
+        public List<Artist> ShowArtists()
+        {
+            return Artists;
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
---
+++ spotivy/Song.cs
@@ -0,0 +1,45 @@
+﻿using static spotivy_app.spotivy.Constants;
+
+namespace spotivy_app.spotivy
+{
+    class Song : IPlayable
+    {
+        public string Title { get; set; }
+        public List<Artist> Artists { get; set; }
+        public Genre SongGenre { get; set; }
+        public int Length { get; set; }
+
+        public Song(string title, List<Artist> artists, Genre genre, int length)
+        {
+            Title = title;
+            Artists = artists;
+            SongGenre = genre;
+            Length = length;
+        }
+
+        public void Play()
+        {
+            Console.Write("Playing song:" + Title + "by");
+            foreach (var artist in Artists)
+            {
+                Console.Write(artist.ToString());
+                if (artist != Artists.Last())
+                {
+                    Console.Write(".");
+                } else Console.WriteLine(", ");
+            }
+        }
+        public void Pause()
+        {
+            Console.WriteLine($"Paused song: " + Title);
+        }
+        public void Next()
+        {
+            Console.WriteLine("Skipping to next song.");
+        }
+        public void Stop()
+        {
+            Console.WriteLine($"Stopped song:" + Title);
+        }
+    }
+}
