c2df2e6184ef2a4d0cb636f69c4f30a711010c51
Yiming-Cao <1192702@student.roc-nijmegen.nl>
2025-06-05 16:02:02 +0200

    master naar dev

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace spotivy_app
+﻿using spotivy_app.spotivy;
+
+namespace spotivy_app
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Messanger Messanger = new Messanger();
+            Messanger.SendMessage("Welcome to Spotivy! This is a simple app to play music, built with C#!");
+            Messanger.FormatMessage("aaaaaaaaaaaaaaaaaaaaaaam                     f", 8, true);
+            Messanger.Conversation("Test", ["bruh", "uhm"], [ [new Option{ Label = "Hello", Action = Hello }, new Option{ Label = "House", Action = House }], [new Option { Label = "House", Action = House }, new Option { Label = "Hello", Action = Hello }] ]);
+        }
+
+        private static void Hello()
+        {
+            Messanger.SendMessage("Hello");
+        }
+        private static void House()
+        {
+            Messanger.FormatMessage(
+                "     ____    " +
+                "    /    \\    " +
+                "   |    |    " +
+                "   |____|    ", 14, true);
         }
     }
 }
---
+++ spotivy/SongCollection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+public class SongCollection
+{
+    public string Title { get; protected set; }
+    protected List<IPlayable> playables;
+
+    public SongCollection(string title)
+    {
+        Title = title;
+        playables = new List<IPlayable>();
+    }
+
+    public virtual List<IPlayable> ShowPlayables()
+    {
+        return playables;
+    }
+
+    public override string ToString()
+    {
+        return $"SongCollection: {Title}, Items: {playables.Count}";
+    }
+}
